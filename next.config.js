2f0772bba3633bec0270ce27d9556bb6d61e4105
Anmol Pandey <anmolpandeyyy@gmail.com>
2025-03-08 19:31:35 +0530

    Configure Next.js for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
+  // Configure the base path if your repository name is not the username.github.io format
+  basePath: '/expense-cal',
+  // Disable image optimization since it's not supported in static exports
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig; 
